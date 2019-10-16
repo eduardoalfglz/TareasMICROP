bb168c574baa9407bccf4ebdb204e95329b01333
eduardoalfglz <43364326+eduardoalfglz@users.noreply.github.com>
2019-10-16 00:06:35 -0600

    funciona subrutina mux_teclado

--- Tarea4/src/EduardoAG_B50203_T4.asm
+++ Tarea4/src/EduardoAG_B50203_T4.asm
@@ -29,6 +29,7 @@ CONT_REB:       ds 1
 CONT_TCL:       ds 1
 PATRON:         ds 1
 BANDERAS:       ds 1
+puertoA:        ds 1
 
 
 NUM_ARRAY:      ds 6
@@ -36,7 +37,7 @@ TECLAS:         db $01,$02,$03,$04,$05,$06,$07,$08,$09,$0B,$00,$0E
 
 
                 org $1100
-MESS1:          fcc "Prueba"
+MESS1:          fcc "Numero %i puerto A:%i"
                 db CR,LF,CR,LF,FIN
                 
                 
@@ -57,7 +58,9 @@ MESS1:          fcc "Prueba"
                 movb #$49, RTICTL       ;FIXME, esto lo pone en 9.26 ms
                 bset CRGINT, $80        ;habilitar interrupciones rti
                 movb #$F0, DDRA
-		cli
+                bset PUCR, $01
+;                bclr RDRIV, $01
+                cli
 
 
 
@@ -83,13 +86,59 @@ mainL:          brset BANDERAS,$04,mainL
 
 
 ;       Subrutina Tarea Teclado
-TAREA_TECLADO:  ldx #$0
+TAREA_TECLADO:  jsr MUX_TECLADO
+		ldx #$0
+
+                ldaa #0
+                ldab puertoA
+                pshd
+                ldaa #0
+                ldab TECLA
+                pshd
                 ldd #MESS1
                 jsr [printf,X]
+                leas 4,sp
                 rts
+
+;       Subrutina MUX_TECLADO
+MUX_TECLADO:    loc
+                ldab #0
+                movb #0,PATRON
+                ldx #TECLAS
+mainloop`       tst PATRON
+                bne p1
+                movb #$EF,PORTA
+                bra READ
+p1:             ldaa #1
+                cmpa PATRON
+                bne p2
+                movb #$DF,PORTA
+                bra READ
+p2:             inca                    ;A=2
+                cmpa PATRON
+                bne p3
+                movb #$BF,PORTA
+                bra READ
+p3:             inca                    ;A=3
+                cmpa PATRON             ;Se detecta cual patron se debe usar en la salida
+                bne nk
+                movb #$7F,PORTA
+read:           brclr PORTA,$01, treturn`       ;se leen las entradas para encontrar la tecla presionada
+                incb
+                brclr PORTA,$02, treturn`
+                incb
+                brclr PORTA,$04, treturn`
+                incb
+                inc PATRON
+                bra mainloop`
+nk              movb #$FF,TECLA                 ;Se guarda la tecla o se retorna FF
+                bra return`
+treturn`        movb PORTA,puertoA
+		movb B,X,TECLA
+return`		rts
 ;       Subrutina formar array
 
-FORMAR_ARRAY:
+FORMAR_ARRAY:   loc
 
                 rts
 
@@ -99,6 +148,7 @@ FORMAR_ARRAY:
 
 ;        subrutina de PHO
                 org $2100
+                loc
 PTH0_ISR:       bset PIFH, $01
                 bset BANDERAS, $04
 ;FIXMe
@@ -111,8 +161,5 @@ INIT_ISR:       bset CRGFLG, $80
                 beq return`
                 dec CONT_REB
 return`         rti
-
-
-
 
 
